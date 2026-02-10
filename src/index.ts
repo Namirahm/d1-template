5bd171ebb9f6c8ffd669f76697f4f6ff0238ef4c
namir <namirahm@gmail.com>
2026-02-10 00:32:29 -0500

    fix for comments table

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { renderHtml } from "./renderHtml";
 
 export default {
 	async fetch(request, env) {
-		const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+		const stmt = await env.DB.prepare("SELECT 1 AS ok").first();
 		const { results } = await stmt.all();
 
 		return new Response(renderHtml(JSON.stringify(results, null, 2)), {
